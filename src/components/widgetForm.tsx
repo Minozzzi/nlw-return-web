238bcfeaa8fa484955ba66f1f0c12b82bdb9b55b
Guilherme Minozzi <minozzi60@gmail.com>
2022-05-23 10:32:07 -0300

    --wip-- [skip ci]

--- src/components/widgetForm.tsx
+++ src/components/widgetForm.tsx
@@ -1,11 +1,38 @@
-import React from "react"
+import React, { useState } from "react"
 import { CloseButton } from "./closeButton"
+import BugIcon from "../assets/bug.svg"
+import IdeiaIcon from "../assets/ideia.svg"
+import ThoughtIcon from "../assets/thought.svg"
 
 const FEEDBACK_TYPES = {
-  
+  bug: {
+    title: "Problema",
+    image: {
+      src: BugIcon,
+      alt: "Imagem de um inseto",
+    },
+  },
+  ideia: {
+    title: "Ideia",
+    image: {
+      src: IdeiaIcon,
+      alt: "Imagem de uma lâmpada",
+    },
+  },
+  other: {
+    title: "Outro",
+    image: {
+      src: ThoughtIcon,
+      alt: "Imagem de um balão de pensamento",
+    },
+  },
 }
 
+type FeedbackType = keyof typeof FEEDBACK_TYPES
+
 export const WidgetForm: React.FC = () => {
+  const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null)
+
   return (
     <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
       <header>
@@ -14,7 +41,17 @@ export const WidgetForm: React.FC = () => {
       </header>
 
       <main className="flex py-8 gap-2 w-full">
-
+        {Object.entries(FEEDBACK_TYPES).map(([type, { title, image }]) => (
+          <button
+            key={type}
+            type="button"
+            onClick={() => setFeedbackType(type as FeedbackType)}
+            className="bg-zinc-800 rounded-lg py-5 w-24 flex-1 flex-col items-center gap-2 border-2 border-transparent hover:border-brand-500 focus:border-brand-500 focus:outline-none"
+          >
+            <img src={image.src} alt={image.alt} />
+            <span>{title}</span>
+          </button>
+        ))}
       </main>
 
       <footer className="text-xs text-neutral-400">
